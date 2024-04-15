b529d3014e2279a98c7f13829064a726e44b8e65
Luccas Arruda <luccas.arruda@somasolucoes.com>
2024-04-15 10:56:53 -0300

    feat: adicionar arquivos de leitura de streams

---
+++ lendo-arquivos-externos/api-star-wars.php
@@ -0,0 +1,3 @@
+<?php
+
+echo file_get_contents('http://receitaws.com.br/v1/cnpj/59291534084328');
---
+++ lendo-arquivos/lendo-arquivos-compactados.php
@@ -0,0 +1,4 @@
+<?php
+
+// echo file_get_contents('zip://lendo-arquivos.rar#arquivo-gerado.txt');
+echo file_get_contents('https://swapi.dev/api/people/1');
